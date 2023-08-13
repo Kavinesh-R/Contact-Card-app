e30d5650803667731743e9d23d5b89766b7f2567
Kavinesh20 <kavineshkd581@gmail.com>
2023-08-13 21:45:37 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const header = () => {
+    return(
+        <div className="">
+            <div className="ui fixed menu">
+                <h2>
+                    Contact Manager
+                </h2>
+            </div>
+
+        </div>
+    )
+}
+
+export default header;
